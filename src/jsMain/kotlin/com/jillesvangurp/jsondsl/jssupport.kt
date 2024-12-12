975ef7720aae4a0fcdc038cb0071874d042ac094
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-12-12 13:57:38 +0100

    add toJsObject() extension function

---
+++ src/jsMain/kotlin/com/jillesvangurp/jsondsl/jssupport.kt
@@ -0,0 +1,39 @@
+package com.jillesvangurp.jsondsl
+
+/**
+ * Extension function to convert the Kotlin JsonDsl to an equivalent Javascript object that you
+ * can pass as a parameter to Javascript libraries.
+ *
+ * This enables you to use JsonDsl as an alternative to complex type mappings for such libraries.
+ * Since JSON is effectively valid javascript, a lot of javascript libraries use some form
+ * of JSON as their input.
+ *
+ * Returns a Javascript object from the Kotlin JsonDsl.
+ */
+fun JsonDsl.toJsObject(): dynamic {
+    fun convert(obj: Any?): dynamic {
+        return when (obj) {
+            null -> null
+            is RawJson -> JSON.parse(obj.value) // Safely parse RawJson as JSON
+            is Number, is Boolean, is String -> obj
+            is Char -> obj.toString() // Serialize Char as String
+            is Map<*, *> -> {
+                val jsObj = js("{}")
+                obj.forEach { (key, value) ->
+                    if (key is String) {
+                        jsObj[key] = convert(value)
+                    }
+                }
+                jsObj
+            }
+            is List<*> -> obj.map { convert(it) }.toTypedArray()
+            is Set<*> -> obj.map { convert(it) }.toTypedArray()
+            is Array<*> -> obj.map { convert(it) }.toTypedArray()
+            is Iterator<*> -> obj.asSequence().map { convert(it) }.toList().toTypedArray()
+            is Iterable<*> -> obj.map { convert(it) }.toTypedArray()
+            is CustomValue<*> -> convert(obj.value)
+            else -> obj.toString() // Fallback for unknown types
+        }
+    }
+    return convert(this)
+}
